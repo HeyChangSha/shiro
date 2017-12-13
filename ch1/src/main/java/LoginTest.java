9de6d0a6de6516952335f76f85af726f44a67338
liuyi <hichangsha@outlook.com>
2017-12-13 13:40:33 +0800

    Initial commit

---
+++ ch1/src/main/java/LoginTest.java
@@ -0,0 +1,40 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author liuyi@stjf.com
+ * @Date 12/13/2017 11:30 AM
+ * @Vendor XMYY Information Technology Co.,Ltd.
+ **/
+
+public class LoginTest {
+
+
+    @Test
+    public void testHelloWorld() {
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager manager = factory.getInstance();
+        SecurityUtils.setSecurityManager(manager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("wang", "456");
+        try {
+            //4、登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            //5、身份验证失败
+        }
+        System.out.println(subject.isAuthenticated());
+        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+        System.out.println(subject.isAuthenticated());
+        //6、退出
+        subject.logout();
+        System.out.println(subject.isAuthenticated());
+    }
+}
